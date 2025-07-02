e8e976a9debb0bd6e3401c6e3c1c0fca5cea930c
mh <minhyung9013@gmail.com>
2025-07-02 11:33:18 +0900

    0702-mh

--- front/my.py
+++ front/my.py
@@ -15,6 +15,8 @@ def show():
     # ▶️ 탭2: 학습 기록 및 미션
     with tab2:
         col12, col22 = st.columns(2)
+        col3, col4 = st.columns(2)
+        col5, col6 = st.columns(2)
 
         with col12:
             with st.expander("📈 나의 학습 통계 보기", expanded=True):
@@ -24,14 +26,14 @@ def show():
                 st.line_chart([10, 15, 20, 22, 30, 34])
 
         with col22:
-            with st.expander("📘 내 단어장 / 오답노트", expanded=True):
+            with st.expander("📘 오답노트", expanded=True):
                 st.write("1. **resolve** - 해결하다")
                 st.write("2. **defeat** - 패배시키다")
                 st.write("3. **clan** - 부족, 문파")
                 if st.button("📤 단어장 내보내기"):
                     st.success("✅ 단어장이 다운로드되었습니다 (예시)")
 
-        col3, col4 = st.columns(2)
+        #col3, col4 = st.columns(2)
 
         with col3:
             with st.expander("🎯 오늘의 학습 미션"):
@@ -49,26 +51,25 @@ def show():
                 )
                 st.button("📖 지금 보러 가기")
 
-    # ▶️ 탭3: 제안 및 피드백
-    with tab3:
-        with st.expander("📅 최근 본 웹툰"):
-            st.write("🖼️ 최근 본 웹툰: **화산귀환**")
-            st.write("📍 마지막 컷: 12번")
-            st.progress(60, text="학습 진행률")
-
-        with st.expander("📊 나의 학습 통계"):
-            st.write("총 학습 컷 수: 34컷")
-            st.write("OCR 결과 확인률: 80%")
-            st.write("정답률: 72%")
-
-            st.subheader("⭐ 관심 웹툰")
-            favorites = st.multiselect(
+        #col5, col6 = st.columns(2)
+        with col5:
+            with st.expander("⭐ 관심 웹툰"):
+            
+                favorites = st.multiselect(
                 "좋아하는 웹툰을 선택하세요",
                 ["유미의 세포들", "김부장", "화산귀환", "스릴러", "입학용병"]
             )
-            st.selectbox("추천 장르 기반", ["살아야 죽는 남자", "입학용병", "무협", "참교육"])
+                st.selectbox("추천 장르 기반", ["살아야 죽는 남자", "입학용병", "무협", "참교육"])
 
-        st.markdown("---")
+        with col6:
+            with st.expander("📅 최근 본 웹툰"):
+                st.write("🖼️ 최근 본 웹툰: **화산귀환**")
+                st.write("📍 마지막 컷: 12번")
+                st.progress(60, text="학습 진행률")
+
+    # ▶️ 탭3: 제안 및 피드백
+    with tab3:
+        
         st.info("마이페이지는 계속 업데이트 중이에요! 원하는 기능이 있다면 아래에 제안해 주세요.")
         feedback = st.text_area("✍️ 기능 제안 또는 개선사항")
         if st.button("제출하기"):
